a1e2db9d357140e9870596f1a2b4c8ab07c734b9
Joao <j.guerreiro@unesp.br>
2024-06-03 17:54:02 +0000

    feat: implement different profiles

--- Api/Program.cs
+++ Api/Program.cs
@@ -9,11 +9,8 @@ builder.Services.ConfigureApp(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
