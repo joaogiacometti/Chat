15d68603c4058e305ee5d18589a4c8ed3a2a5504
João <j.guerreiro@unesp.br>
2024-06-12 16:19:49 +0000

    feat: separate app.vue and navbar

--- Presentation/src/App.vue
+++ Presentation/src/App.vue
@@ -1,31 +1,14 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+</script>
+
 <template>
   <v-app>
-    <v-app-bar>
-      <template v-slot:prepend>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      </template>
-
-      <v-app-bar-title>Chat</v-app-bar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" temporary>
-      <v-list>
-        <v-list-item title="Chats" prepend-icon="mdi-chat" to="/chats" />
-
-        <v-list-item title="Login" prepend-icon="mdi-login" to="/login" />
-
-        <v-list-item title="Register" prepend-icon="mdi-account-plus" to="/register" />
-      </v-list>
-    </v-navigation-drawer>
-
+    <Navbar />
     <v-main>
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-
-const drawer = ref(false)
-</script>
+<script setup></script>
---
+++ Presentation/src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(false)
+</script>
+
+<template>
+  <v-app-bar>
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    </template>
+
+    <v-app-bar-title>Chat</v-app-bar-title>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" temporary>
+    <v-list>
+      <v-list-item title="Chats" prepend-icon="mdi-chat" to="/chats" />
+
+      <v-list-item title="Login" prepend-icon="mdi-login" to="/login" />
+
+      <v-list-item title="Register" prepend-icon="mdi-account-plus" to="/register" />
+    </v-list>
+  </v-navigation-drawer>
+</template>
